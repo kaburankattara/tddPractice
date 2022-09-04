530268155817e9bf1c7c5cf9e7eb853508a40fd3
kaburankattara <kaburankattara@gmail.com>
2022-09-05 08:36:39 +0900

    インターフェースを変更したことによるコンパイルエラーを解決 テスト結果：レッド

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -3,12 +3,12 @@ package money;
 public class Dollar {
     int amount;
 
-    public Dollar(int amount){
+    public Dollar(int amount) {
         this.amount = amount;
     }
 
-    void times(int multiplier){
-        amount = amount * multiplier;
+    Dollar times(int multiplier){
+        amount *= multiplier;
+        return null;
     }
-
 }
