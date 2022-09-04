a634dcf8d68a8e28ed8259a4e9e2465932e5d85e
tmpuser-at-498588106976 <>
2022-09-05 07:53:47 +0900

    テストを追加。 Dollarが状態変化しているため、テスト結果がレッド。

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -9,6 +9,8 @@ public class MoneyTest{
     public void testMultiplication(){
         Dollar five = new Dollar(5);
         five.times(2);
-        assertEquals(10,five.amount);
+        assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15, five.amount);
     }
 }
